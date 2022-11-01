4eddb794b6e156e9ee1d3c4a7fb45d8c34a1c33b
Seytkhan Balikbaev <seytkhanbalikbaev@Seytkhan.local>
2022-11-02 00:17:03 +0600

    completed second serverless function

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -1,1 +1,33 @@
-const result = document.querySelector('.result')
+const result = document.querySelector(".result");
+
+const url = `/api/2-basic-api`;
+
+const fetchData = async () => {
+  try {
+    const { data } = await axios(url);
+    const products = data
+      .map((product) => {
+        const {
+          id,
+          image: { url },
+          name,
+          price,
+        } = product;
+        return `
+        <article key="${id}" class="product">
+          <img src="${url}" alt="${name}" />
+          <div class="info">
+            <h5>${name}</h5>
+            <h5 class="price">$${price}</h5>
+          </div>
+        </article>
+     `;
+      })
+      .join("");
+    result.innerHTML = products;
+  } catch (error) {
+    result.innerHTML = `<h4>There was an error. Please try again later</h4>`;
+  }
+};
+
+fetchData();
